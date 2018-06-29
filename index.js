47b4b679921cf303650e47f316b6f41fbb1f3626
James Stuckey <jstuckey@rutgers.edu>
2018-06-29 12:36:21 -0400

    first commit

--- index.js
+++ index.js
@@ -11,31 +11,57 @@ const { dbConnect } = require('./db-mongoose');
 const app = express();
 
 app.use(
-  morgan(process.env.NODE_ENV === 'production' ? 'common' : 'dev', {
-    skip: (req, res) => process.env.NODE_ENV === 'test'
-  })
+    morgan(process.env.NODE_ENV === 'production' ? 'common' : 'dev', {
+        skip: (req, res) => process.env.NODE_ENV === 'test'
+    })
 );
 
 app.use(
-  cors({
-    origin: CLIENT_ORIGIN
-  })
+    cors({
+        origin: CLIENT_ORIGIN
+    })
 );
 
+app.get('/api/cheeses', (req, res, next) => {
+    const cheeses = {
+        cheeses: [
+            'Bath Blue',
+            'Barkham Blue',
+            'Buxton Blue',
+            'Cheshire Blue',
+            'Devon Blue',
+            'Dorset Blue Vinney',
+            'Dovedale',
+            'Exmoor Blue',
+            'Harbourne Blue',
+            'Lanark Blue',
+            'Lymeswold',
+            'Oxford Blue',
+            'Shropshire Blue',
+            'Stichelton',
+            'Stilton',
+            'Blue Wensleydale',
+            'Yorkshire Blue'
+        ]
+    };
+
+    res.json(cheeses);
+});
+
 function runServer(port = PORT) {
-  const server = app
-    .listen(port, () => {
-      console.info(`App listening on port ${server.address().port}`);
-    })
-    .on('error', err => {
-      console.error('Express failed to start');
-      console.error(err);
-    });
+    const server = app
+        .listen(port, () => {
+            console.info(`App listening on port ${server.address().port}`);
+        })
+        .on('error', err => {
+            console.error('Express failed to start');
+            console.error(err);
+        });
 }
 
 if (require.main === module) {
-  dbConnect();
-  runServer();
+    dbConnect();
+    runServer();
 }
 
 module.exports = { app };
